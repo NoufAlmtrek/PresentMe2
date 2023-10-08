22dbd81a33f44cdc81d3b3cb1721e8d1714b1ad0
NoufAlmtrek <noufalmtrek@gmail.com>
2023-10-08 17:47:09 +0300

    Initial Commit

---
+++ PresentMe/PresentMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PresentMeApp.swift
+//  PresentMe
+//
+//  Created by Nouf Almtrek on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct PresentMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
